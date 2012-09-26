8557e6be150290ad5b0681862b91a04462a91c19
lifish <u1502073@hotmail.com>
2012-09-26 22:11:58 +0800

    build article models make 3 models to 1 (Teacher, Student, Achievement --> Article)

--- article/models.py
+++ article/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class Teacher(models.Model):
-	pass
-	
-class Student(models.Model):
-	pass
-	
-class Achievement(models.Model):
-	pass
+class Article(models.Model):
+	ARTICLE_TYPE = (
+		('LE','lecturer'),
+		('ST','student'),
+		('AC','achievement'),
+	)
+	subject = models.CharField(max_length=200)	
+	name = models.CharField(max_length=200)
+	company = models.CharField(blank=True, max_length=200)
+	title = models.CharField(blank=True, max_length=200)
+	content = models.TextField()
+	group = models.CharField(max_length=2, choices=ARTICLE_TYPE)
